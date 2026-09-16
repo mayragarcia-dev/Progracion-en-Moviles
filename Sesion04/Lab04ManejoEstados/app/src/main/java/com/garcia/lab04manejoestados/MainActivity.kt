0a1a545ae8f488bd55fa8b77e7b30a2ad9162656
mayragarcia-dev <mayjulisarojas@gmail.com>
2026-09-16 02:20:36 -0500

    Agregar vista previa de pantalla de tareas

--- Sesion04/Lab04ManejoEstados/app/src/main/java/com/garcia/lab04manejoestados/MainActivity.kt
+++ Sesion04/Lab04ManejoEstados/app/src/main/java/com/garcia/lab04manejoestados/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.garcia.lab04manejoestados.ui.theme.Lab04ManejoEstadosTheme
 
@@ -225,5 +226,13 @@ fun PantallaTareas() {
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewPantallaTareas() {
+    MaterialTheme {
+        PantallaTareas()
     }
 }
