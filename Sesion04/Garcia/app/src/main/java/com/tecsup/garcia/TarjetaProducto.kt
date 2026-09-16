bd1bf8b71589062924d6486066e8a3aa2aa90c8f
mayragarcia-dev <mayjulisarojas@gmail.com>
2026-09-16 16:53:58 -0500

    Mejorar TarjetaProducto mostrando nombre precio y cantidad

--- Sesion04/Garcia/app/src/main/java/com/tecsup/garcia/TarjetaProducto.kt
+++ Sesion04/Garcia/app/src/main/java/com/tecsup/garcia/TarjetaProducto.kt
@@ -3,13 +3,24 @@ package com.tecsup.garcia
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 @Composable
 fun TarjetaProducto(producto: Producto) {
 
     Card {
-        Text(
-            text = producto.nombre
-        )
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = producto.nombre
+            )
+
+            Text(
+                text = "S/ ${producto.precio} x ${producto.cantidad}"
+            )
+        }
     }
 }
