2ee41f355969d89204e504e016dc7a6a2d629d3b
mayragarcia-dev <mayjulisarojas@gmail.com>
2026-09-16 16:45:33 -0500

    Corregir lista de productos

--- Sesion04/Garcia/app/src/main/java/com/tecsup/garcia/MainActivity.kt
+++ Sesion04/Garcia/app/src/main/java/com/tecsup/garcia/MainActivity.kt
@@ -7,11 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.snapshots.mutableStateListOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.tecsup.garcia.ui.theme.GarciaTheme
-
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
