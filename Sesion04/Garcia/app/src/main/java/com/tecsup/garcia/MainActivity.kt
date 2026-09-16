2165b0c0b53a8df97999cfc219d4ccc8ef5ede4d
mayragarcia-dev <mayjulisarojas@gmail.com>
2026-09-16 17:13:39 -0500

    Centrar mensaje cuando el carrito esta vacio

--- Sesion04/Garcia/app/src/main/java/com/tecsup/garcia/MainActivity.kt
+++ Sesion04/Garcia/app/src/main/java/com/tecsup/garcia/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.tecsup.garcia.ui.theme.GarciaTheme
@@ -127,6 +128,20 @@ fun PantallaCarrito() {
                     .weight(1f),
                 verticalArrangement = Arrangement.spacedBy(12.dp)
             ) {
+
+                if (productos.isEmpty()) {
+                    item {
+                        Box(
+                            modifier = Modifier.fillMaxWidth(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = "No hay productos en el carrito"
+                            )
+                        }
+                    }
+                }
+
                 items(productos) { producto ->
                     TarjetaProducto(
                         producto = producto,
