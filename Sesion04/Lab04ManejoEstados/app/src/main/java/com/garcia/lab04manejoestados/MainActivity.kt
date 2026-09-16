15eca1692ba0ca7b75c38afbf657f0d679ee0413
mayragarcia-dev <mayjulisarojas@gmail.com>
2026-09-15 23:52:01 -0500

    Agregar ejercicio ContadorRoto sin remember

---
+++ Sesion04/Lab04ManejoEstados/app/src/main/java/com/garcia/lab04manejoestados/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.garcia.lab04manejoestados
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.garcia.lab04manejoestados.ui.theme.Lab04ManejoEstadosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Lab04ManejoEstadosTheme {
+                ContadorRoto()
+            }
+        }
+    }
+}
+
+@Composable
+fun ContadorRoto() {
+    var contador = 0
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Contador: $contador")
+
+        Button(
+            onClick = {
+                contador++
+            }
+        ) {
+            Text("Incrementar")
+        }
+    }
+}
