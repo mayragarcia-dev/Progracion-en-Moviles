639d839485a77c6a6d0267697ea470e63d16821e
mayragarcia-dev <mayjulisarojas@gmail.com>
2026-09-16 16:48:16 -0500

    Preparar imports para lista de productos

--- Sesion04/Garcia/app/src/main/java/com/tecsup/garcia/MainActivity.kt
+++ Sesion04/Garcia/app/src/main/java/com/tecsup/garcia/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.tecsup.garcia
 
 import android.os.Bundle
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.Alignment
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
