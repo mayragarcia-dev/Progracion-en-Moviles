7445e9ccc61e51f0ec8d86301f4f1f5351fc7d94
mayragarcia-dev <mayjulisarojas@gmail.com>
2026-09-22 07:51:26 -0500

    Sesion05: configura NavController, NavHost y rutas con argumentos

--- Sesion05/NavLab/app/src/main/java/com/tuapp/navlab/MainActivity.kt
+++ Sesion05/NavLab/app/src/main/java/com/tuapp/navlab/MainActivity.kt
@@ -4,44 +4,61 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.tuapp.navlab.navigation.Screen
+import com.tuapp.navlab.screens.DetailScreen
+import com.tuapp.navlab.screens.HomeScreen
+import com.tuapp.navlab.screens.ProfileScreen
 import com.tuapp.navlab.ui.theme.NavLabTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             NavLabTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigation()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AppNavigation() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NavLabTheme {
-        Greeting("Android")
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+
+        composable(Screen.Home.route) {
+            HomeScreen(navController)
+        }
+
+        composable(Screen.List.route) {
+            ListScreen(navController)
+        }
+
+        composable(Screen.Profile.route) {
+            ProfileScreen(navController)
+        }
+
+        composable(Screen.Detail.route) { backStackEntry ->
+
+            val itemId =
+                backStackEntry.arguments?.getString("itemId")?.toIntOrNull() ?: 0
+
+            DetailScreen(
+                navController = navController,
+                itemId = itemId
+            )
+        }
     }
 }
