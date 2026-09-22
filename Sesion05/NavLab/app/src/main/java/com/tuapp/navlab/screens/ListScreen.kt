c606d4fc115f30b6b327c53b6c807d7ddb6f12a6
mayragarcia-dev <mayjulisarojas@gmail.com>
2026-09-22 08:58:53 -0500

    Sesion05: organiza la pantalla de lista y navegacion al detalle

--- Sesion05/NavLab/app/src/main/java/com/tuapp/navlab/screens/ListScreen.kt
+++ Sesion05/NavLab/app/src/main/java/com/tuapp/navlab/screens/ListScreen.kt
@@ -1,5 +1,4 @@
-package com.tuapp.navlab
-
+package com.tuapp.navlab.screens
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
