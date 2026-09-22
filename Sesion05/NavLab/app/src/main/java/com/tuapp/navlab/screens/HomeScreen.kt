26fb92da6b052696b2c66fca246f9e3b4b02cfba
mayragarcia-dev <mayjulisarojas@gmail.com>
2026-09-22 07:03:17 -0500

    Sesion05: implementa HomeScreen como pantalla Compose y agrega navegacion hacia ListScreen

--- Sesion05/NavLab/app/src/main/java/com/tuapp/navlab/screens/HomeScreen.kt
+++ Sesion05/NavLab/app/src/main/java/com/tuapp/navlab/screens/HomeScreen.kt
@@ -1,4 +1,19 @@
 package com.tuapp.navlab.screens
 
-class HomeScreen {
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.tuapp.navlab.navigation.Screen
+
+@Composable
+fun HomeScreen(navController: NavController) {
+
+    Button(
+        onClick = {
+            navController.navigate(Screen.List.route)
+        }
+    ) {
+        Text("Ir a Lista")
+    }
 }
