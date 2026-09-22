82e4c08e55fa2f389134aab6b5b31c01a985870f
mayragarcia-dev <mayjulisarojas@gmail.com>
2026-09-22 07:50:36 -0500

    Sesion05: agrega dependencias de navegacion

--- Sesion05/NavLab/app/build.gradle.kts
+++ Sesion05/NavLab/app/build.gradle.kts
@@ -36,20 +36,34 @@ android {
 }
 
 dependencies {
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    // Compose BOM: SOLO una vez
     implementation(platform(libs.androidx.compose.bom))
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    // Material Icons
+    implementation("androidx.compose.material:material-icons-extended")
+
+    // Compose
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
+
+    // AndroidX
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Tests
     testImplementation(libs.junit)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
+
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+
+    // Debug
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
