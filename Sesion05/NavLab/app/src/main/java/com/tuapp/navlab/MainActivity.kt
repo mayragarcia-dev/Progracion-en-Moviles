9f4c31ed3cfad8c64eb83d53e1b8666b566658d2
mayragarcia-dev <mayjulisarojas@gmail.com>
2026-09-22 22:36:23 -0500

    Sesion05: actualiza actividad principal para integrar la navegacion

--- Sesion05/NavLab/app/src/main/java/com/tuapp/navlab/MainActivity.kt
+++ Sesion05/NavLab/app/src/main/java/com/tuapp/navlab/MainActivity.kt
@@ -4,15 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.tuapp.navlab.navigation.Screen
-import com.tuapp.navlab.screens.DetailScreen
-import com.tuapp.navlab.screens.HomeScreen
-import com.tuapp.navlab.screens.ListScreen
-import com.tuapp.navlab.screens.ProfileScreen
+import com.tuapp.navlab.navigation.AppNavigation
 import com.tuapp.navlab.ui.theme.NavLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,41 +17,6 @@ class MainActivity : ComponentActivity() {
             NavLabTheme {
                 AppNavigation()
             }
-        }
-    }
-}
-
-@Composable
-fun AppNavigation() {
-
-    val navController = rememberNavController()
-
-    NavHost(
-        navController = navController,
-        startDestination = Screen.Home.route
-    ) {
-
-        composable(Screen.Home.route) {
-            HomeScreen(navController)
-        }
-
-        composable(Screen.List.route) {
-            ListScreen(navController)
-        }
-
-        composable(Screen.Profile.route) {
-            ProfileScreen(navController)
-        }
-
-        composable(Screen.Detail.route) { backStackEntry ->
-
-            val itemId =
-                backStackEntry.arguments?.getString("itemId")?.toIntOrNull() ?: 0
-
-            DetailScreen(
-                navController = navController,
-                itemId = itemId
-            )
         }
     }
 }
